c9ee92f976e0cf1bb09c54de8306a1fba204652f
Siaoja <160692147@qq.com>
2020-07-27 20:08:28 +0800

    [GuessNumberGame][spike lu] refactor: convert constant 4 to a class field ANSWER_LENGTH.

--- src/main/java/example/GuessNumberAnswerGenerator.java
+++ src/main/java/example/GuessNumberAnswerGenerator.java
@@ -5,13 +5,16 @@ import java.util.Random;
 import java.util.Set;
 
 public class GuessNumberAnswerGenerator implements Generator{
+
+    private static final int ANSWER_LENGTH = 4;
+
     @Override
     public String generate() {
         Random random = new Random();
         Set<Integer> numbersSet = new HashSet<>();
         StringBuilder guessNumber = new StringBuilder();
 
-        while (numbersSet.size() != 4){
+        while (numbersSet.size() != ANSWER_LENGTH){
             numbersSet.add(random.nextInt(10));
         }
         for(Integer number : numbersSet){
